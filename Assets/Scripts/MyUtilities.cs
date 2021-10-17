3fdf89936e38b5ef5ca7b7b5f2328c344ce79cab
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-17 02:05:59 -0300

    Utilities for the desing

---
+++ Assets/Scripts/MyUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MyUtilities
+{
+    public static bool Contains(LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
