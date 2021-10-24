029ca7b8f07e58f833ddeaafb9331345c5e92e42
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-24 10:37:31 -0300

    Added enemy stats

---
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float movementForce;
+    public float distanceTracking;
+    public float distanceToAttack;
+    public float attackDelay;
+    public float attackRadiusArea;
+    public float damage;
+    public float knockbackForce;
+}
