23b8b76d36c6ef44a64ebb0543fd414c961e7a1e
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-24 10:37:18 -0300

    Added enemy movement system

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] EnemyStats stats;
+    float auxTimer;
+    float auxDistante;
+
+    public void Move(Vector2 position)
+    {
+        float distance = Vector2.Distance(transform.position, position);
+        if(auxDistante != distance)
+        {
+            auxDistante = distance;
+            auxTimer = 0f;
+        }
+        //
+        auxTimer += Time.deltaTime * stats.movementForce / auxDistante;
+        transform.position = Vector2.Lerp(transform.position, position, auxTimer);
+    }
+}
