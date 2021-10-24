8a4d04d44938c910053f3c88025ec253a5d1dd92
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-24 10:34:00 -0300

    Added enemy manager class

---
+++ Assets/Scripts/EnemyManager/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> enemiesPrefab;
+    [SerializeField] UnityEvent switchGatesOpen;
+    [HideInInspector] public int enemiesAmount;
+    GameManager gameManager;
+
+    void Awake() 
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void GenerateEnemies()
+    {
+        switchGatesOpen?.Invoke();
+        enemiesAmount++;
+        //Cuando genero el enemigo puedo enviarle la referencia del player a travez del gameManager
+    }
+
+    public void IfEnemyDie()
+    {
+        enemiesAmount--;
+        if(enemiesAmount <= 0)
+            switchGatesOpen?.Invoke();
+    }
+}
