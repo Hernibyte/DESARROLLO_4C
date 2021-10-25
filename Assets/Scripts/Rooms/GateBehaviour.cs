f9c3af9b50dbf9d778e88c6a47c9e4f06a798662
FacundoPonce <mrfacu125@gmail.com>
2021-10-25 11:14:58 -0300

    Gates animation working.

--- Assets/Scripts/Rooms/GateBehaviour.cs
+++ Assets/Scripts/Rooms/GateBehaviour.cs
@@ -7,7 +7,19 @@ public class GateBehaviour : MonoBehaviour
     [HideInInspector] public Vector2 teleportPosition;
     [HideInInspector] public bool isGateOpen;
     [SerializeField] LayerMask playerMask;
+    Animator gateAnimator;
 
+    private void Start()
+    {
+        gateAnimator = GetComponentInChildren<Animator>();
+    }
+    public void UpdateGateAnimation()
+    {
+        if (gateAnimator == null)
+            return;
+
+        gateAnimator.SetBool("IsOpen", isGateOpen);
+    }
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(isGateOpen)
