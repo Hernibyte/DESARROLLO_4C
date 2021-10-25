fbe86856ff400def5c220a755f19ab17c2c46a4e
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-25 02:06:30 -0300

    Added menu scene

---
+++ Assets/Scripts/Menu/UIMenuManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject uiCreditsObject;
+    [SerializeField] GameObject uiOptionsObject;
+
+    void Awake()
+    {
+        uiCreditsObject.SetActive(false);
+        uiOptionsObject.SetActive(false);
+    }
+
+    public void Play()
+    {
+        ChangeScene("Game");
+    }
+
+    public void Options(bool state)
+    {
+        uiOptionsObject.SetActive(state);
+    }
+
+    public void Credits(bool state)
+    {
+        uiCreditsObject.SetActive(state);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        StartCoroutine(LoadingScene(sceneName));
+    }
+
+    IEnumerator LoadingScene(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        yield return operation.isDone;
+    }
+}
