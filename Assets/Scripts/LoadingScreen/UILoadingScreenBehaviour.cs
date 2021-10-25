1437027090430b7dc3ca9c85bd39f9f9c29a82c3
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-25 04:42:43 -0300

    Added loading screen

---
+++ Assets/Scripts/LoadingScreen/UILoadingScreenBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UILoadingScreenBehaviour : MonoBehaviour
+{
+    [SerializeField] GameObject mText;
+    [SerializeField] UnityEvent close;
+    bool closeLoadingScreen;
+
+    void Start()
+    {
+        mText.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && closeLoadingScreen)
+        {
+            close?.Invoke();
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Close()
+    {
+        closeLoadingScreen = true;
+        mText.SetActive(true);
+    }
+}
