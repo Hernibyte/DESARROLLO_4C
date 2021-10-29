264f688101c3aa0883940cd02fff27eb6be5ca78
Gossa Hernan <hernanagustin.g24@hotmail.com>
2021-10-29 16:54:39 -0300

    Provisional camera bug fixed

--- Assets/Scripts/Camera/CameraBehaviour.cs
+++ Assets/Scripts/Camera/CameraBehaviour.cs
@@ -12,13 +12,17 @@ public class CameraBehaviour : MonoBehaviour
 
     IEnumerator SetLook(Vector2 point)
     {
-        float aux = 0f;
-        Vector3 positionAux = transform.position;
-        for(int i = 0; i < 10; i++)
-        {
-            transform.position =  Vector3.Lerp(positionAux, new Vector3(point.x, point.y, -10), aux);
-            aux += 0.1f;
-            yield return new WaitForFixedUpdate();
-        }
+        //float aux = 0f;
+        //Vector3 positionAux = transform.position;
+        transform.position = new Vector3(point.x, point.y, -10);
+        //for(int i = 0; i < 20; i++)
+        //{
+        //    if(aux >= 1.0f)
+        //        break;
+        //    transform.position =  Vector3.LerpUnclamped(positionAux, new Vector3(point.x, point.y, -10), aux);
+        //    aux += 0.1f;
+        //    yield return new WaitForFixedUpdate();
+        //}
+        yield return null;
     }
 }
