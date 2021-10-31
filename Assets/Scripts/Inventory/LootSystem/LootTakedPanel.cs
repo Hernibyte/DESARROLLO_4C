d5e30d1e5a27b4e8d6fcfddb69219c3b8f6223bd
FacundoPonce <mrfacu125@gmail.com>
2021-10-30 23:04:35 -0300

    Created prefab of card loot.

--- Assets/Scripts/Inventory/LootSystem/LootTakedPanel.cs
+++ Assets/Scripts/Inventory/LootSystem/LootTakedPanel.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class LootTakedPanel : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] LootManager lootReference;
+    [SerializeField] TextMeshProUGUI nameCard;
 
     private void OnEnable()
     {
@@ -15,8 +17,17 @@ public class LootTakedPanel : MonoBehaviour
         lootReference.lootTaken -= ShowLootPanel;
     }
 
+    public void SetCardNameToUI()
+    {
+        if (lootReference == null || lootReference.GetCardsAviable().cardList.Count <= 0)
+            return;
+
+        nameCard.text = lootReference.GetCardsAviable().cardList[lootReference.GetLastCardTaked()].data.name;
+    }
+
     public void ShowLootPanel()
     {
+        SetCardNameToUI();
         animator.SetBool("ShowLootPanel", true);
     }
 
